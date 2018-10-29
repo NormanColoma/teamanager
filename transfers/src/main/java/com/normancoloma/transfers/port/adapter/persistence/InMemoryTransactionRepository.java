ce98d6fb4cc9123a6f9f8743e9535f9432964ea4
Norman Coloma García <ua.norman@gmail.com>
2018-10-29 23:41:15 +0100

    Fixing null pointer

--- transfers/src/main/java/com/normancoloma/transfers/port/adapter/persistence/InMemoryTransactionRepository.java
+++ transfers/src/main/java/com/normancoloma/transfers/port/adapter/persistence/InMemoryTransactionRepository.java
@@ -4,12 +4,13 @@ import com.normancoloma.transfers.domain.model.transaction.Transaction;
 import com.normancoloma.transfers.domain.model.transaction.TransactionRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class InMemoryTransactionRepository implements TransactionRepository {
 
-    private List<Transaction> transactions;
+    private List<Transaction> transactions = new ArrayList<>();
 
     @Override
     public void save(Transaction transaction) {
