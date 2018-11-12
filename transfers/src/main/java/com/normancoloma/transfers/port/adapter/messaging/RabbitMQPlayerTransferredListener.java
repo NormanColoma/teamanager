2b9cd8f1646330aa581a2e1d27c31bcddaa41ecc
Norman Coloma García <ua.norman@gmail.com>
2018-11-12 23:23:57 +0100

    Mapping domain object from notification to command

--- transfers/src/main/java/com/normancoloma/transfers/port/adapter/messaging/RabbitMQPlayerTransferredListener.java
+++ transfers/src/main/java/com/normancoloma/transfers/port/adapter/messaging/RabbitMQPlayerTransferredListener.java
@@ -12,33 +12,27 @@ import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.Set;
-import java.util.UUID;
 
 @Component
 @AllArgsConstructor
 public class RabbitMQPlayerTransferredListener implements ExchangeListener {
     private final GenerateTransferTransaction generateTransferTransaction;
+    private final ObjectMapper objectMapper;
 
     @RabbitListener(
             bindings = @QueueBinding(value = @Queue(value = "player.transfers"),
                     exchange = @Exchange(value = "player.events", type = ExchangeTypes.FANOUT))
     )
     @Override
-    public void filter(Notification notification) throws IOException {
+    public void filter(Notification notification) {
         if(listenToMessagesOfType().contains(notification.getType())) {
-            LinkedHashMap domainEvent = (LinkedHashMap) notification.getDomainEvent();
+            GenerateTransferTransactionCommand generateTransferTransactionCommand = objectMapper
+                    .convertValue(notification.getDomainEvent(), GenerateTransferTransactionCommand.class);
 
-            GenerateTransferTransactionCommand command = GenerateTransferTransactionCommand.builder()
-                    .playerId(UUID.fromString(domainEvent.get("playerId").toString()))
-                    .teamId(UUID.fromString(domainEvent.get("teamId").toString()))
-                    .quantity(Float.parseFloat(domainEvent.get("quantity").toString()))
-                    .build();
-            generateTransferTransaction.execute(command);
+            generateTransferTransaction.execute(generateTransferTransactionCommand);
         }
     }
 
